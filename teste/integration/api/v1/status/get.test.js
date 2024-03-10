7a3c181f8f1723bfdd7d7e6680bb030d0272fd74
arthurfran <65544920+arthurfran@users.noreply.github.com>
2024-03-10 18:28:45 +0000

    adiciona banco de dados `sql`

--- teste/integration/api/v1/status/get.test.js
+++ teste/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
 test("teste de integração saída 200", async () => {
-  const request = await fetch("http://localhost:3000/api/v1/status");
-  expect(request.status).toBe(200);
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 });
